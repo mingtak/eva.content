7cc0e7c9d7e7d1dbd9f4060ecd7c9dad048189ab
Andy Fang <andy@mingtak.com.tw>
2018-09-30 13:21:46 +0800

    update 0930 13:20

--- src/eva/content/browser/views.py
+++ src/eva/content/browser/views.py
@@ -13,6 +13,33 @@ import logging
 
 
 logger = logging.getLogger("EVA EVENT")
+
+
+class Sign(BrowserView):
+    """ Sign """
+
+    template = ViewPageTemplateFile("template/sign.pt")
+
+    def __call__(self):
+        portal = api.portal.get()
+        context = self.context
+        request = self.request
+
+#        import pdb; pdb.set_trace()
+        if not bool(request.form.has_key('name') and request.form.has_key('tel') and request.form.has_key('email') and request.form.has_key('where')):
+            return self.template()
+
+        name = request.form.get('name')
+        tel = request.form.get('tel')
+        email = request.form.get('email')
+        where = request.form.get('where')
+
+        if not bool( name and tel and email and where ):
+            return 1 # 缺資料
+        else:
+            return 'true' # 成功
+
+
 class CoverView(BrowserView):
     """ Cover View """
 
