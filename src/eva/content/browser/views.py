116716c2ece3a37238a4cb6edd9f5a712f9d4633
Andy Fang <andy@mingtak.com.tw>
2018-09-30 17:03:13 +0800

    update 0930 17:03

--- src/eva/content/browser/views.py
+++ src/eva/content/browser/views.py
@@ -8,11 +8,29 @@ from plone.protect.interfaces import IDisableCSRFProtection
 from zope.interface import alsoProvides
 from DateTime import DateTime
 
+import json
 import random
 import logging
 
 
 logger = logging.getLogger("EVA EVENT")
+
+
+class Again(BrowserView):
+    """ Again """
+
+    template = ViewPageTemplateFile("template/again.pt")
+
+    def __call__(self):
+        portal = api.portal.get()
+        context = self.context
+        request = self.request
+
+        if 'eva-macau.playgroup.com.tw' not in request.get('HTTP_REFERER'):
+            request.response.redirect(portal.absolute_url())
+            return
+
+        return self.template()
 
 
 class Sign(BrowserView):
@@ -25,7 +43,10 @@ class Sign(BrowserView):
         context = self.context
         request = self.request
 
-#        import pdb; pdb.set_trace()
+        if 'eva-macau.playgroup.com.tw' not in request.get('HTTP_REFERER'):
+            request.response.redirect(portal.absolute_url())
+            return
+
         if not bool(request.form.has_key('name') and request.form.has_key('tel') and request.form.has_key('email') and request.form.has_key('where')):
             return self.template()
 
@@ -36,8 +57,15 @@ class Sign(BrowserView):
 
         if not bool( name and tel and email and where ):
             return 1 # 缺資料
-        else:
-            return 'true' # 成功
+
+        result = json.loads(portal['result'].description)
+        email = email.lower()
+        if result.has_key(email):
+            return 2 # 已登錄過
+
+        result[email] = [name, tel, where, DateTime().strftime('%Y/%m/%d %H:%M')]
+        portal['result'].description = json.dumps(result)
+        return 'true' # 成功
 
 
 class CoverView(BrowserView):
@@ -54,3 +82,16 @@ class CoverView(BrowserView):
         context = self.context
         request = self.request
         return self.template()
+
+
+class UserList(BrowserView):
+    """ User List """
+
+    template = ViewPageTemplateFile("template/user_list.pt")
+
+    def __call__(self):
+        portal = api.portal.get()
+        context = self.context
+        request = self.request
+        return self.template()
+
